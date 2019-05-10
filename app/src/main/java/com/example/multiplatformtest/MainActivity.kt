39e5891f837445df3b68280399563f5ccca4870b
Shawn Vega <shawn@Te2-MacBook-Pro.local>
2019-05-10 10:04:14 -0700

    initial commit

---
+++ app/src/main/java/com/example/multiplatformtest/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.multiplatformtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import org.kotlin.mpp.mobile.createApplicationScreenMessage
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.main_text).text = createApplicationScreenMessage()
+    }
+}
