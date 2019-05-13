bd0385782167290cf36d569e1d226308a03ca4cc
Shawn Vega <shawnsvega@gmail.com>
2019-05-13 11:33:18 -0700

    added get full thumbor url function to android app

--- app/src/main/java/com/example/multiplatformtest/MainActivity.kt
+++ app/src/main/java/com/example/multiplatformtest/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.multiplatformtest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 import org.kotlin.mpp.mobile.CrossTest
 import org.kotlin.mpp.mobile.createApplicationScreenMessage
+import org.kotlin.mpp.mobile.getFullThumborURL
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +15,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         findViewById<TextView>(R.id.main_text).text = createApplicationScreenMessage()
+        button.setOnClickListener {
+            processInput()
+        }
+    }
+
+    fun processInput() {
+        val baseUrl = textInputUrlBase.text.toString()
+        val relativeUrl = textInputUrlRelative.text.toString()
+        val widthText = textInputWidth.text
+        val heightText = textInputHeight.text
+        try {
+            val width = widthText.toString().toInt()
+            val height = heightText.toString().toInt()
+            main_text.text = getFullThumborURL(baseUrl, relativeUrl, width, height)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    companion object {
+        const val THUMBOR_BASE_URL = "https://img.te2.io"
     }
 }
